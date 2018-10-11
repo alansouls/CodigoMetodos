a58fe1cbabb77f550f1bd576103306194cde7cba
Alan Maia <maiaalan@hotmail.com>
2018-10-10 22:13:32 -0300

    Add files via upload

--- gauss.py
+++ gauss.py
@@ -2,7 +2,21 @@ import sys
 import numpy as np
 import solveTriang as sT
 
+def piv(A,b,l,p):
+	n = A.shape[0]
+	maximo,index = 0,l
+	for i in range(l,n):
+		if maximo < abs(A[i,l]):
+			maximo,index = abs(A[i,l]),i
+	if l!=index:
+		A[[l,index]] = A[[index,l]]
+		b[0,l],b[0,index] = b[0,index],b[0,l]
+		p += 1
+	return A,p
+
+
 def gauss(A,b):
+	p = 0
 	print("A matriz A será convertida em matriz triangular: ")
 	print("Matriz A: ")
 	print(A)
@@ -10,6 +24,10 @@ def gauss(A,b):
 	print(np.transpose(b))
 	n = A.shape[0]
 	for k in range(0,n-1):
+		print("Matriz nao pivotada :\n",A)
+		A,p = piv(A,b,k,p)
+		print("Matriz pivotada em :", k)
+		print(A)
 		for i in range(k,n-1):
 			m = (-1)*(A[i+1,k])/A[k,k]
 			for j in range(k,n):
@@ -19,11 +37,14 @@ def gauss(A,b):
 	print(A)
 	print("Novo vetor b: ")
 	print(np.transpose(b))
-	det = 0
+	det = 1
+
 	for i in range(0,n):
-		det += A[i,i]
+		det *= A[i,i]
 	x = sT.solveTriang(A,b,True,False)	
 	x = x[:,None]
+	if(p % 2 != 0):
+		det = -det
 	print("Det A = ", det)
 	return x
 A=np.array(np.mat(sys.argv[1]),dtype=float)
---
+++ gaussJordan.py
@@ -0,0 +1,50 @@
+import sys 
+import numpy as np
+
+def piv(A,b,l,p = 0):
+	n = A.shape[0]
+	maximo,index = 0,l
+	for i in range(l,n):
+		if maximo < abs(A[i,l]):
+			maximo,index = abs(A[i,l]),i
+	if l!=index:
+		A[[l,index]] = A[[index,l]]
+		b[0,l],b[0,index] = b[0,index],b[0,l]
+		p += 1
+	return A
+
+def gaussJordan(A,b):
+	print("Matriz A é :\n",A)
+	print("Vetor b é : \n",np.transpose(b))
+	b0 = b
+	n = A.shape[0]
+	I = np.zeros(A.shape)
+	for i in range(0,n):
+		I[i,i] = 1
+	AI = np.array([A,I])
+	for i in range(0,n):
+		A = piv(AI[0],b,i)
+		AI[0] = A
+		b[0,i] /= AI[0,i,i]
+		AI[:,i] = AI[:,i]/AI[0,i,i] 
+		for k in range(0,n):
+			if k != i:
+				m = -AI[0,k,i]/AI[0,i,i]
+				AI[:,k] += AI[:,i]*m
+				b[0,k] += b[0,i]*m
+	print("A nova matriz A é: \n",AI[0])
+	print("A inversa de A é : \n",AI[1])
+	print("O vetor solução x é : \n",np.transpose(b))
+	print("O vetor de residuos é: \n",(AI[0].dot(np.transpose(b)))-np.transpose(b0))
+	return b
+
+
+
+
+
+
+
+A=np.array(np.mat(sys.argv[1]),dtype=float)
+b=np.array(np.mat(sys.argv[2]),dtype=float)
+
+gaussJordan(A,b)
