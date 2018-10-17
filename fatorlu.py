3dfd22b32a2e79cf9f2f413727f4166da047319f
Alan Maia <maiaalan@hotmail.com>
2018-10-17 09:50:21 -0300

    Add files via upload

--- fatorlu.py
+++ fatorlu.py
@@ -4,45 +4,50 @@ import solveTriang as sT
 
 
 def piv(A,l,p):
-	n = A.shape[0]
-	maximo,index = 0,l
-	for i in range(l,n):
-		if maximo < abs(A[i,l]):
-			maximo,index = abs(A[i,l]),i
-	if l!=index:
-		A[[l,index]] = A[[index,l]]
-		p += 1
-	return p
+    n = A.shape[0]
+    maximo,index = 0,l
+    for i in range(l,n):
+        if maximo < abs(A[i,l]):
+            maximo,index = abs(A[i,l]),i
+    if l!=index:
+        A[[l,index]] = A[[index,l]]
+        p += 1
+    return p
 
 
 def fatorLU(A,b):
-	p = 0
-	print("Matriz A é :\n", A)
-	print("Vetor b é :\n", np.transpose(b))
-	n = A.shape[0]
-	L = np.identity(n)
-	U = A
-	for k in range(0,n):
-		#p = piv(U,k,p)
-		for i in range(k,n-1):
-			m = -U[i+1,k]/U[k,k]
-			U[i+1] += U[k]*m
-			L[i+1,k] = -m
-	print("Matriz U:\n", U)
-	print("Matriz L:\n", L)
-	y = sT.solveTriang(L,b,False,False)
-	y = y.reshape(1,n)
-	print("Vetor y:\n", np.transpose(y))
-	x = sT.solveTriang(U,y,True,False)
-	x = x.reshape(1,n)
-	print("TESTE : \n", L @ U)
-	det = 1
-	for i in range(0,n):
-		det *= U[i,i]
-	det *= (-1**p)
-	print("Det de A = ", det)
-	print("Vetor solução: \n",np.transpose(x))
-	print("Vetor resíduos:\n", L@np.transpose(y) - np.transpose(b))
+    p = 0
+    print("Matriz A é :\n", A)
+    print("Vetor b é :\n", np.transpose(b))
+    n = A.shape[0]
+    L = np.zeros(A.shape)
+    U = A
+    for k in range(0,n):
+        p = piv(U,k,p)
+        for i in range(k,n-1):
+            m = -U[i+1,k]/U[k,k]
+            U[i+1] += U[k]*m
+            L[i+1,k] = -m
+        
+        
+    for i in range(0,n):
+        piv(L,i,p)
+    
+    print("Matriz U:\n", U)
+    print("Matriz L:\n", L)
+    y = sT.solveTriang(L,b,False,False)
+    y = y.reshape(1,n)
+    print("Vetor y:\n", np.transpose(y))
+    x = sT.solveTriang(U,y,True,False)
+    x = x.reshape(1,n)
+    print("TESTE : \n", L @ U)
+    det = 1
+    for i in range(0,n):
+        det *= U[i,i]
+    det *= (-1**p)
+    print("Det de A = ", det)
+    print("Vetor solução: \n",np.transpose(x))
+    print("Vetor resíduos:\n", L@np.transpose(y) - np.transpose(b))
 
 
 A=np.array(np.mat(sys.argv[1]),dtype=float)
--- gaussSeidel.py
+++ gaussSeidel.py
@@ -3,12 +3,51 @@ import numpy as np
 
 
 def checkMatrix(A):
-	n = A.shape[0]
-	for i in range(n):
-		s = 0
-		for k in range(n):
-			if i != k:
-				s += abs(A[i,k])
-		if abs(A[i,i]) <= s:
-			return False
-	return True 
+    n = A.shape[0]
+    for i in range(n):
+        s = 0
+        for k in range(n):
+            if i != k:
+                s += abs(A[i,k])
+        if abs(A[i,i]) <= s:
+            return False
+    return True 
+
+def gaussSeidel(A,b,e,maxIter = 300):
+    print("Matriz A:\n",A) 
+    it = 0
+    print("Vetor b:\n",np.transpose(b))
+    n = A.shape[0]
+    x = np.zeros(b.shape)
+    print("Vetor X0 \n", np.transpose(x))
+    print("Verificando se a matriz obedece o critério das linhas...")
+    if checkMatrix(A) == False:
+        print("A matriz pode não convergir")
+    else:
+        print("A matriz possui diagonal estritamente dominante")
+
+ 
+    for p in range(0,maxIter):
+         it += 1
+         xAnt = np.copy(x)
+         for i in range(0,n):
+             s = b[0,i]
+             for k in range(0,n):
+                 if(i != k):
+                     s -= A[i,k]*x[0,k]
+             x[0,i] = s/A[i,i]
+         d = abs(x - xAnt)
+         print("D\n",d)
+         maxDR = np.amax(d)/np.amax(abs(x))
+         if maxDR < e:
+              break
+    print(A @ np.transpose(x) - np.transpose(b))         
+    print("O vetor solução aproximado é \n", np.transpose(x))
+    print("Numero de iterações: ", it)
+    return x
+
+
+A=np.array(np.mat(sys.argv[1]),dtype=float)
+b=np.array(np.mat(sys.argv[2]),dtype=float)
+e = float(sys.argv[3])
+gaussSeidel(A,b,e,)
